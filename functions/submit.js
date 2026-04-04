4b2ed5c4ad93ef6da0e9cab097129b2ce5f122fd
modernheresy <34037373+modernheresy@users.noreply.github.com>
2026-04-04 13:29:15 +0300

    Add files via upload

--- functions/submit.js
+++ functions/submit.js
@@ -1,15 +1,13 @@
 export async function onRequestPost(context) {
   try {
-    const input = await context.request.formData();
-    const email = input.get('email');
+    const data = await context.request.formData();
+    const email = data.get('email');
 
     if (!email) {
       return new Response("Email is required", { status: 400 });
     }
 
-    // This is where you would eventually plug in an Email API (like Mailgun or SendGrid)
-    // For now, we will redirect the user to a "Success" page to deliver the PDF.
-    
+    // This redirects the user to your ALREADY CREATED success page
     return Response.redirect(`${new URL(context.request.url).origin}/success.html`, 303);
 
   } catch (err) {
